74065718aa0b58402b38078dd17e7894fd9ef1e9
UrsuGheorghe <gheorgheursu@gmail.com>
2020-08-14 14:59:23 +0300

    Entity relation remodeling and added FlyWay for migration and verioning DB

--- src/main/java/com/passengertransportation/demo/model/Passenger.java
+++ src/main/java/com/passengertransportation/demo/model/Passenger.java
@@ -3,6 +3,7 @@ package com.passengertransportation.demo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -11,13 +12,31 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Embeddable
+@Entity
+@Table(name = "passengers")
 public class Passenger {
 
+    @Id
+    @Column(name = "passenger_id")
+    private Long id;
+
+
+    @Column(name = "passenger_name")
+    @NonNull
     private String name;
+
+    @Column(name = "passport_data")
+    @NonNull
     private String passportData;
+
+    @Column(name = "birth_date")
     @DateTimeFormat(pattern = "dd.MM.yyyy")
+    @NonNull
     private Date birthDate;
+
+    @Column(name = "luggage_weight")
     private int luggageWeight;
 
+    @OneToOne(mappedBy = "passenger")
+    private Ticket ticket;
 }
