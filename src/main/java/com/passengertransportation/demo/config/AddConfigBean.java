bef7c20441cf7c9611f42fb8a92939ecb3ee9544
UrsuGheorghe <gheorgheursu@gmail.com>
2020-09-17 12:12:54 +0300

    Changed ports From container 8080:8282 Machine 8888

--- src/main/java/com/passengertransportation/demo/config/AddConfigBean.java
+++ src/main/java/com/passengertransportation/demo/config/AddConfigBean.java
@@ -24,5 +24,6 @@ public class AddConfigBean {
     private String db_password;
     @Value("${db_name}")
     private String db_name;
-
+    @Value("${server_port}")
+    private String server_port;
 }
